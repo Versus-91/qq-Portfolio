f441f6ffbc2e1d5777b91875fa4d6c6dc524b1c7
Ghader <gharibyan.ghader@gmail.com>
2025-12-03 11:47:13 +0100

    overhauling project descriptions

--- ui/app/pages/index.vue
+++ ui/app/pages/index.vue
@@ -6,17 +6,18 @@
             </template>
             <template #default>
                 <div class="flex flex-col md:flex-row">
-                    <div class="w-full md:w-2/3">
+                    <div class="w-full md:w-2/3 ">
                         {{ item.description }}
-                        <h2 class="mt-2 text-lg font-medium text-heading" v-if="item.features?.length ?? 0 > 0">Features
+                        <h2 class=" text-lg font-medium text-heading my-2 mt-2" v-if="item.features?.length ?? 0 > 0">
+                            Highlights
                         </h2>
-                        <ul class="text-left text-body  list-disc pl-5">
+                        <ul class="text-left text-body  list-disc pl-5 my-2">
                             <li v-for="f in item.features">
                                 <span>
                                     {{ f }}
                                 </span>
                             </li>
-                            <li class="text-body my-5"><span class="font-bold">Technologies:</span> {{ item.stack }}
+                            <li class="text-body"><span class="font-bold">Stack:</span> {{ item.stack }}
                             </li>
                         </ul>
                     </div>
@@ -30,7 +31,8 @@
             <template #footer>
                 <UButton color="neutral" icon="i-lucide-square-code" variant="outline" class="mx-1 dark:text-white-400 "
                     :to="item.sourceLink" target="_blank">Source</UButton>
-                <UButton color="primary" icon="i-lucide-play" :to="item.previewLink" variant="outline" target="_blank">
+                <UButton v-if="!!item.previewLink" color="primary" icon="i-lucide-play" :to="item.previewLink"
+                    variant="outline" target="_blank">
                     Demo</UButton>
             </template>
         </UCard>
@@ -45,32 +47,35 @@ const works_content =
             title: 'Machine Learning in Web',
             description: `A web-based platform for building machine learning pipelines with integrated tools for PFI, PDP, PCA, t-SNE, and autoencoders. Supports data visualization, explainable AI, and dimensionality reduction. Scales to large datasets via TU Dresden's HPC infrastructure.`,
             previewLink: 'https://ml-sandbox-dun.vercel.app/',
-            features: ['Training 14 classification and regression models inside browser.', 'Prallel coordinate plot and correlation matrix analysis',
-                'PCA and t-SNE dimentionality reduction methods with bplot, scree plot and pca ans t-sne matrix visulaizayion'],
+            features: ['14+ ML models training directly in browser (no backend needed)',
+                'Explainable AI suite: PDP, PFI, and feature contribution analysis',
+                'Dimensionality reduction: PCA (biplots/scree), t-SNE, autoencoders',
+                'Interactive visualizations: Parallel coordinates, correlation matrices, model insights',
+                'HPC integration: Scales to large datasets via TU Dresden infrastructure',
+                'Full client-side execution using Pyodide + TensorFlow.js'],
             images: ['https://carddecks.blob.core.windows.net/portfolio/Screenshot 2025-11-24 100327.jpg', 'https://carddecks.blob.core.windows.net/portfolio/ppc.jpg',
                 'https://carddecks.blob.core.windows.net/portfolio/Screenshot 2025-11-24 101328.jpg', 'https://carddecks.blob.core.windows.net/portfolio/pfi.jpg'
             ],
             stack: "Vue.js 3, Pyodide, Bulma CSS, Scikit-learn, Plotly.js, HighCharts, TensorFlow.js",
-            sourceLink: '#',
+            sourceLink: '',
             tags: ['Web', 'Machine Learning'],
             palceholderImage: 'images/logo.png'
         },
         {
             title: 'Realtor App',
-            description: `A full-stack real estate management platform with a web frontend built in Vue.js, a ASP.NET Core backend, a SQL Server database, and a cross-platform mobile app developed with Flutter.`,
+            description: `A full-stack real estate management platform with a web frontend built in Vue.js, a ASP.NET Core 8 backend, and a SQL Server database.`,
             previewLink: 'https://card-decks.azurewebsites.net/',
             sourceLink: 'https://github.com/Versus-91/RealtorSuite',
-            stack: "ASP.NET Core 8, Vue.js 2, SLQ Server, Swagger UI, Bulma CSS, Bootstrap 4",
+            stack: "ASP.NET Core 8, abp, Vue.js 2, SLQ Server, Swagger UI, Bulma CSS, Bootstrap 4",
             features: ["ASP.NET 8 backend development for property listing platform",
                 "RESTful API design with JWT authentication and authorization",
-                "Device-optimized image rendering service for faster page loads",
                 "Azure cloud deployment: App Service, SQL Database, Key Vault",
+                "Secure configuration management using Azure Key Vault",
+                "Device-optimized image rendering service for faster page loads",
                 "Entity Framework Core integration with advanced query optimization",
                 "Role-based access control and secure API endpoint protection",
-                "Comprehensive API documentation with Swagger/OpenAPI",
                 "Automated CI/CD pipeline implementation for Azure deployment",
-                "Performance optimization through caching and database tuning",
-                "Secure configuration management using Azure Key Vault"],
+            ],
             tags: ['Web'],
             images: ['https://carddecks.blob.core.windows.net/portfolio/Home.jpg', 'https://carddecks.blob.core.windows.net/portfolio/show.jpg'
             ],
@@ -78,13 +83,13 @@ const works_content =
         },
         {
             title: 'TDD app with Django 5.0',
-            stack: "Django 5.0, Selenium automated test framework, pytest",
+            stack: "Django 5.0, Selenium automated test framework, pytest, SQLite",
             description: `Developed a CRUD application with TDD methodology.`,
-            features: ["Full CRUD operations implemented with Django Class-Based Views",
+            features: ["Full CRUD operations implemented with Django",
                 "one-time code login without traditional signup process",
                 "Test-Driven Development methodology using pytest test suite",
                 "Selenium automated browser testing for end-to-end validation",
-                "Comprehensive unit tests for models, views, and forms",],
+                "Unit tests for models, views, and forms"],
             palceholderImage: 'images/map.png',
             sourceLink: "https://github.com/Versus-91/PYTDD",
             tags: ['Web']
@@ -92,9 +97,9 @@ const works_content =
         {
             title: 'The Blackbox of Machine Learning',
             description: `
-                        Explored the "black box" nature of machine learning algorithms by applying PDP,PFI, and SHAP model interpretability techniques to explain ML models' decissions.`
+                        Explored the blackbox nature of machine learning algorithms by applying PDP,PFI, and SHAP model interpretability techniques to explain ML models' decissions.`
             ,
-            previewLink: '#',
+            previewLink: '',
             features: ["Explored PDP, PFI, and SHAP for explaining black box models' predictions and enhancing transparency",
                 "Simulated 8 different scenarios and visualized the methods' explanations with the true DGP",
                 "Benchmarked the methods on 8 public datasets and 12 ML models"],
@@ -114,18 +119,18 @@ const works_content =
             title: 'Distance Aware Vision Transformer',
             description: `Improved model accuracy for cancer detection in whole slide images by designing a distance-aware Vision Transformer (ViT) that incorporated spatial relationships between patches into the self-attention mechanism.
                         `,
-            previewLink: '#',
+            previewLink: '',
             palceholderImage: 'images/t.webp',
             stack: "PyTorch",
             sourceLink: 'https://github.com/Versus-91/SpatiallyAwareTransformerNetwork',
             tags: ['Deep Learning']
         }
         , {
-            title: 'Reinforcement Learning Pac-man',
+            title: 'Deep Reinforcement Learning Pac-man',
             description: `Using Deep Reinforcement Learning models to learn how to play Pac-man based on
                                 interaction wiith the game
                                 and feedback received.`,
-            previewLink: '#',
+            previewLink: '',
             stack: "PyTorch, Pygame",
             sourceLink: 'https://github.com/Versus-91/pacman_final',
             palceholderImage: 'images/drl.webp',
