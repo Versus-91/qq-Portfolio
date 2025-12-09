1add3ec42b29f96ec7eb4a6df949031b6ea02bb2
Ghader <gharibyan.ghader@gmail.com>
2025-12-09 16:00:00 +0100

    cv section vertical spaces fix

--- ui/app/components/Resume.vue
+++ ui/app/components/Resume.vue
@@ -2,7 +2,7 @@
     <UCard id="tttt">
         <template #default>
             <h1 class="uppercase font-bold border-b-2 border-b-blue-400 py-1 mb-2">Work Experience</h1>
-            <div class="flex flex-row w-full mb-2 text-left">
+            <div class="flex flex-row w-full mb-1 text-left">
                 <div class="basis-2/3 border-cyan-300">
                     <h4 class="text-blue-600 font-bold dark:text-cyan-400">Software Engineer</h4>
                 </div>
@@ -10,9 +10,9 @@
                     <h4 class="text-blue-600 font-bold">10.2022 - 03.2025</h4>
                 </div>
             </div>
-            <div class="flex pl-4 mb-5 pb-5">
+            <div class="flex pl-4 pb-2">
                 <ol class="list-disc">
-                    <li>Engineered a client-side ML platform using Pyodide, allowing users to build and train
+                    <li>Created a client-side ML platform using Pyodide, allowing users to build and train
                         models directly in the browser without code.</li>
                     <li>Designed a responsive UI with Vue.js 3 and Bulma CSS, achieving a Lighthouse score of 91</li>
                     <li>Integrated 15+ ML algorithms into the platform via Pyodide and Scikit-learn</li>
@@ -29,7 +29,7 @@
                 </div>
             </div>
             <div class="flex pl-4">
-                <ol class="list-disc">
+                <ol class="list-disc pb-2">
                     <li>Optimized SPA applications' performance through tree shaking, lazy-loading modules</li>
                     <li>Identified and optimized inefficient EF Core and SQL queries using SSMS</li>
                     <li>Designed and deployed a standalone data-entry application (Angular 14, ASP.NET Zero)
@@ -43,7 +43,7 @@
             <h1 class="uppercase font-bold border-b-2 border-b-blue-400 py-1 mb-2">Education</h1>
             <Experience :title="'M.Sc. in Computational Modeling and Simulation, TU Dresden'"
                 :date="'10.2022 - 03.2025'">
-                <ul class="list-disc pl-4">
+                <ul class="list-disc pl-4 pb-2">
                     <li>
                         Coursework: Scientific Programming, Design Pattern and frameworks, User Interface
                         Design, Data Visualization (D3.js), Scalable Data Engineering
